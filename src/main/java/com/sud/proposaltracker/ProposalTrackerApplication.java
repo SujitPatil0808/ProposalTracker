6222fafc7ad4fa04f7b1cbf7bcaf68620f88bf4c
SujitPatil <patilsujit205@gmail.com>
2024-06-23 18:03:11 +0530

    Proposal Project Created

---
+++ src/main/java/com/sud/proposaltracker/ProposalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sud.proposaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sud/proposaltracker/ProposalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sud.proposaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
